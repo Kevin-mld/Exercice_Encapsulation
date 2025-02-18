4b746d5141d445effd9b7a0ab258bc380986380a
Kevin-mld <kevinmalandain61@gmail.com>
2025-02-18 10:43:26 -0500

    Fini

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,27 +2,72 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur;
+        Personnage monstre;
+
         public Form1()
         {
             InitializeComponent();
+            joueur = new Personnage();
+            joueur.PointDeVieMaximum = 10;
+            joueur.PointDeVie = 10;
+            joueur.PuissanceAttaque = 1;
+            monstre = new Personnage();
+            monstre.PointDeVieMaximum = 30;
+            monstre.PointDeVie = 30;
+            monstre.PuissanceAttaque = 3;
+
+            ActualiserAffichage();
         }
 
-        Personnage joueur = new Personnage();
-        Personnage monstre = new Personnage(); 
+        
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
+            monstre.PointDeVie -= joueur.PuissanceAttaque;
+            joueur.PointDeVie -= monstre.PuissanceAttaque;
 
+            ActualiserAffichage();
+            if (joueur.EstMort)
+            {
+                MessageBox.Show("Vous êtes mort");
+            }
+
+            if (monstre.EstMort)
+            {
+                MessageBox.Show("Vous avez vaincus le Dragon");
+            }
+            
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
+            joueur.PointDeVie += 5;
+            joueur.PointDeVie -= monstre.PuissanceAttaque;
 
+            ActualiserAffichage();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            joueur.PuissanceAttaque += 4;
+            joueur.PointDeVie -= monstre.PuissanceAttaque;
 
+            ActualiserAffichage();
+
+            if (joueur.EstMort)
+            {
+                MessageBox.Show("Vous êtes mort");
+            }
+        }
+
+        private void ActualiserAffichage()
+        {
+            lbl_pointsVieJoueur.Text = $"Points de vie: {joueur.PointDeVie}";
+            lbl_pointsVieDragon.Text = $"Points de vie {monstre.PointDeVie}";
+
+            lbl_puissanceAttaqueJoueur.Text = $"Puissance d'attaque: {joueur.PuissanceAttaque}";
+            lbl_puissanceAttaqueDragon.Text = $"Puissance d'attaque: {monstre.PuissanceAttaque}";
         }
     }
 }
--- PO1_Encapsulation/Personnage.cs
+++ PO1_Encapsulation/Personnage.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace PO1_Encapsulation
 {
-    internal class Personnage
+    public class Personnage
     {
+        int pointDeVie;
+        int puissanceAttaque;
+        int pointDeVieMaximum;
+
+        public int PointDeVie { get => pointDeVie; 
+            set {
+                if (value < 0)
+                {
+                    pointDeVie = 0;
+                }
+
+                else if (value > PointDeVieMaximum)
+                {
+                    pointDeVie = PointDeVieMaximum;
+                }
+
+                else 
+                {
+                    pointDeVie = value;
+                }
+            } 
+        }
+
+        public bool EstMort
+        {
+            get
+            {
+                if (pointDeVie == 0)
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public int PuissanceAttaque { get => puissanceAttaque; set => puissanceAttaque = value; }
+        public int PointDeVieMaximum { get => pointDeVieMaximum; set => pointDeVieMaximum = value; }
     }
 }
